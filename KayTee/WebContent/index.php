69b5bae15cecc430b68c5d9def6b47359f3e4225
Charles McKelvey <csmckelvey@gmail.com>
2015-09-20 21:20:17 -0500

    [SYNCHING WITH CHANGES MADE ON WEB SERVER]

--- KayTee/WebContent/index.php
+++ KayTee/WebContent/index.php
@@ -1,33 +1,8 @@
 <!DOCTYPE HTML>
 <HTML lang="en-US">
-	<HEAD>
-		<TITLE>Kay-Tee Boutique</TITLE>
-		
-		<META charset="UTF-8">
-		<META name="language" content="EN">
-		<META name="keywords" content="baby, child, children, clothing, toys, consignment, cheap,
-		                               sale, buy, sell, new, used, homemade, local, apple valley, 
-									   victorville, hesperia, adelanto, california, ca, high desert">
-		<META name="description" content="A homegrown local store selling all things baby and child 
-		                                  related. Buy new, used, or sell your own items on consignment!">
-		
-		<META name="url" content="http://www.kayteebabyboutique.com">
-		<META name="identifier-URL" content="http://www.kayteebabyboutique.com">
-		
-		<META name="copyright" content="Kay-Tee Baby Boutique">
-		<META name="pagename" content="Homepage of Kay-Tee Baby Boutique">
-		<META name="author" content="Charles McKelvey, https://www.linkedin.com/pub/charles-mckelvey/73/964/62">
-		
-		<SCRIPT src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></SCRIPT>
-		<SCRIPT src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></SCRIPT>
-		<LINK href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" />
-		<LINK href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" />
-		
-		<SCRIPT src="js/global.js"></SCRIPT>
-		<LINK rel="icon" href="images/logos/icon.png" />
-		<LINK rel="stylesheet" type="text/css" href="css/global.css" />
-		<LINK rel="stylesheet" type="text/css" href="css/navbar.css" />
-	</HEAD>
+
+	<?php #printHeader("Homepage of Kay-Tee Baby Boutique", "Kay-Tee Baby Boutique Homepage", "js1.js,js2.js", "css1.css,css2.css"); ?>
+	<?php echo("Hello"); ?>
 	
 	<BODY id="body" class="adjustHeight" >
 		<DIV class="container">
