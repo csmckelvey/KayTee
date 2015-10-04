f704e00e587df0fee5b970a867fd37ad24dacd49
Charles McKelvey <csmckelvey@gmail.com>
2015-10-03 19:34:18 -0500

    Removing obsolete file - Code moved to map.js

--- KayTee/WebContent/js/contact.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	var mapCanvas = document.getElementById('map-canvas');
-	var mapOptions = {
-		center: new google.maps.LatLng(34.4998999, -117.186725),
-		zoom: 15,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	var map = new google.maps.Map(mapCanvas, mapOptions);
-});
